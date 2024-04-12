cea4244cd224b1d856bbc922744f27cf3c49e522
BradBurr-GH <45184565+BradBurr-Github@users.noreply.github.com>
2024-04-12 02:02:45 -0600

    code from JQuery UI

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Modal-Dialog
+# SkyCast
+
+SkyCast: A 5-day weather forecasting app.
+ 
+## Image of My Website
+ 
+<img src="./assets/images/webpage.png" alt="SkyCast webpage image"/>
+ 
+## Visit My Website
+ 
+[My Website](https://bradburr-github.github.io/Sky-Cast/)
+ 
+ ## License
+
+This project is currently not licensed.
---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Handle form submission
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  // Here you can perform further actions with the username and password
+  console.log("Username:", username);
+  console.log("Password:", password);
+  // For demo purposes, close the modal after submitting the form
+  modal.style.display = "none";
+});
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Handle form submission
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var name = document.getElementById("name").value;
+  var password = document.getElementById("password").value;
+  // You can perform validation or further processing here
+  console.log("Name:", name);
+  console.log("Password:", password);
+  modal.style.display = "none";
+});
